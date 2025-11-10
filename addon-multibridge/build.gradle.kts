2823181dee4e1b8961af237295bf14b678a64b86
TonyBucket <111670594+TonyBucket@users.noreply.github.com>
2025-11-10 19:29:37 +0700

    Integrate MultiBridge with DiscordSRV webhook flow

---
+++ addon-multibridge/build.gradle.kts
@@ -0,0 +1,51 @@
+import org.gradle.api.file.DuplicatesStrategy
+
+plugins {
+    id("java")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+repositories {
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://nexus.scarsz.me/content/groups/public/")
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.21.1-R0.1-SNAPSHOT")
+    compileOnly(project(":"))
+    implementation("net.dv8tion:JDA:5.0.0-beta.20")
+}
+
+tasks {
+    jar {
+        enabled = false
+    }
+
+    shadowJar {
+        archiveBaseName.set("DiscordSRV-MultiBridge")
+        archiveClassifier.set("")
+        archiveVersion.set("")
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+        val relocationPrefix = "com.wrs.multibridge.libs"
+        relocate("net.dv8tion.jda", "$relocationPrefix.jda")
+        relocate("club.minnced.discord.webhook", "$relocationPrefix.discord.webhook")
+        relocate("com.neovisionaries.ws.client", "$relocationPrefix.neovisionaries.ws")
+        relocate("com.iwebpp.crypto", "$relocationPrefix.iwebpp.crypto")
+        relocate("okhttp3", "$relocationPrefix.okhttp3")
+        relocate("okio", "$relocationPrefix.okio")
+    }
+
+    assemble {
+        dependsOn(shadowJar)
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+}
