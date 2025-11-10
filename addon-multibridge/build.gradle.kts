4d2290d9297cf0e03e36e0798c7e8e02205e9a8b
TonyBucket <111670594+TonyBucket@users.noreply.github.com>
2025-11-10 16:07:10 +0700

    Fix bot config parsing for MultiBridge

---
+++ addon-multibridge/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("java")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+repositories {
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://nexus.scarsz.me/content/groups/public/")
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.21.1-R0.1-SNAPSHOT")
+    compileOnly(project(":"))
+    implementation("net.dv8tion:JDA:5.0.0-beta.20")
+}
+
+tasks.jar {
+    archiveBaseName.set("DiscordSRV-MultiBridge")
+    from("src/main/resources") {
+        include("plugin.yml", "config.yml")
+    }
+}
